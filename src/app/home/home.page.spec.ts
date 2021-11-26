2d9edf628be8e02d3975f5c53878f9ef75945829
Matt Beattie <mattgbeattie@gmail.com>
2021-11-26 13:32:18 -0500

    adds capability for unit tests, with coverage enforcement

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
@@ -7,16 +10,18 @@ describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [HomePage],
+        imports: [IonicModule.forRoot(), TranslateModule.forRoot(), HttpClientTestingModule],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(HomePage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/present-one-contest/present-one-contest.page.spec.ts
+++ src/app/present-one-contest/present-one-contest.page.spec.ts
@@ -7,16 +7,22 @@ describe('PresentOneContestPage', () => {
   let component: PresentOneContestPage;
   let fixture: ComponentFixture<PresentOneContestPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PresentOneContestPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [PresentOneContestPage],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(PresentOneContestPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(PresentOneContestPage);
+      component = fixture.componentInstance;
+      component.contest = {
+        getVotesAllowed: () => {},
+      } as any;
+      component.home = {} as any;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
