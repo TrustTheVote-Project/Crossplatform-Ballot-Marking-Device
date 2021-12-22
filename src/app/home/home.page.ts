1587037274d3437b98e0b0f8e75947c4a4045e06
Bret Schuhmacher <>
2021-12-22 11:21:23 -0500

    added submit ballot button, first cut at CVR, regexp to writein

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,261 +13,244 @@ import { Election } from '../../classes/Election';
 import { WriteinPopupPage } from '../writein-popup/writein-popup.page';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+   selector: 'app-home',
+   templateUrl: 'home.page.html',
+   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  @ViewChild('mySlider') slides: IonSlides;
+   @ViewChild('mySlider') slides: IonSlides;
 
-  sliderConfig = {
-    effect: 'cube',
-    autoHeight: true,
-  };
+   sliderConfig = {
+      effect: 'cube',
+      autoHeight: true,
+   };
 
-  public xml = '';
-  public xmlFile = '/assets/data/64K_1Contest.xml';
-  public electionContestNames: string[];
-  public currentContest: number;
-  public title: string;
-  public titleTwo: string;
-  public description: string;
-  public name: string;
-  public language: string;
-  private election: Election;
-  private modal: ModalController;
-  private edFiles: string[];
+   public xml = '';
+   public xmlFile = '/assets/data/64K_1Contest.xml';
+   public electionContestNames: string[];
+   public currentContest: number;
+   public title: string;
+   public titleTwo: string;
+   public description: string;
+   public name: string;
+   public language: string;
+   private election: Election;
+   private modal: ModalController;
+   private edFiles: string[];
 
-  constructor(public modalController: ModalController, private readonly http: HttpClient, private translate: TranslateService) {
-    SplashScreen.show({
-      showDuration: 2000,
-      autoHide: true,
-    });
-    this.modal = modalController;
-  }
+   constructor(public modalController: ModalController, private readonly http: HttpClient, private translate: TranslateService) {
+      SplashScreen.show({
+         showDuration: 2000,
+         autoHide: true,
+      });
+      this.modal = modalController;
+   }
 
-  ngOnInit() {
-    this.initializeApp();
-    this.currentContest = 1;
-  }
+   ngOnInit() {
+      this.initializeApp();
+      this.currentContest = 1;
+   }
 
-  initializeApp() {
-    this.getDeviceLanguage();
-    this.getEDFiles();
-    console.log('EDFiles is :' + this.edFiles);
-    this.openXML();
-  }
+   initializeApp() {
+      this.getDeviceLanguage();
+      this.getEDFiles();
+      console.log('EDFiles is :' + this.edFiles);
+      this.openXML();
+   }
 
-  openXML() {
-    //alternate data files... need to be able to select which to use
-    this.election = new Election(this.http, this.xmlFile, this);
-    //from the device... TO-DO...later
-    //this.election = new Election( this.http, '/assets/data/64K_1Contest.xml', this);
-    //                  this.election = new Election( this.http, '/assets/data/results-06037-2017-03-07.xml', this);
-    //this.election = new Election( this.http, '/assets/data/LA_County_Reference.xml', this);
-    ////this.election = new Election(this.http, '/assets/data/results-06037-2016-11-08.xml');
-  }
+   openXML() {
+      //alternate data files... need to be able to select which to use
+      this.election = new Election(this.http, this.xmlFile, this);
+      //from the device... TO-DO...later
+      //this.election = new Election( this.http, '/assets/data/64K_1Contest.xml', this);
+      //                  this.election = new Election( this.http, '/assets/data/results-06037-2017-03-07.xml', this);
+      //this.election = new Election( this.http, '/assets/data/LA_County_Reference.xml', this);
+      ////this.election = new Election(this.http, '/assets/data/results-06037-2016-11-08.xml');
+   }
 
-  async openIonModal(data: any) {
-    const modal = await this.modalController.create({
-      component: ModalPopupPage,
-      componentProps: {
-        title: data.title,
-        body: data.body,
-      },
-    });
-    return await modal.present();
-  }
+   async openIonModal(data: any) {
+      const modal = await this.modalController.create({
+         component: ModalPopupPage,
+         componentProps: {
+            title: data.title,
+            body: data.body,
+         },
+      });
+      return await modal.present();
+   }
 
-  async voteReview(): Promise<void> {
-    const voteReviewPopupContent = {
-      scrollToContest: 0,
-      home: this,
-      election: this.election,
-      title: 'Vote Review',
-      body: 'election review goes here',
-    };
+   async voteReview(): Promise<void> {
+      const voteReviewPopupContent = {
+         scrollToContest: 0,
+         home: this,
+         election: this.election,
+         title: 'Vote Review',
+         body: 'election review goes here',
+      };
 
-    const voteReviewModal = await this.modal.create({
-      component: VoteReviewPage,
-      componentProps: voteReviewPopupContent,
-    });
-    await voteReviewModal.present();
-    voteReviewModal.onDidDismiss();
-  }
+      const voteReviewModal = await this.modal.create({
+         component: VoteReviewPage,
+         componentProps: voteReviewPopupContent,
+      });
+      await voteReviewModal.present();
+      voteReviewModal.onDidDismiss();
+   }
 
-  public async voteReviewSpecificContest(contestNum: number): Promise<void> {
-    console.log('home.page::voteReviewSpecificContest - should center on contest ' + contestNum);
-    const voteReviewPopupContent = {
-      scrollToContest: contestNum,
-      home: this,
-      election: this.election,
-      title: 'Vote Review',
-      body: 'election review goes here',
-    };
+   public async voteReviewSpecificContest(contestNum: number): Promise<void> {
+      console.log('home.page::voteReviewSpecificContest - should center on contest ' + contestNum);
+      const voteReviewPopupContent = {
+         scrollToContest: contestNum,
+         home: this,
+         election: this.election,
+         title: 'Vote Review',
+         body: 'election review goes here',
+      };
 
-    const voteReviewModal = await this.modal.create({
-      component: VoteReviewPage,
-      componentProps: voteReviewPopupContent,
-    });
-    await voteReviewModal.present();
+      const voteReviewModal = await this.modal.create({
+         component: VoteReviewPage,
+         componentProps: voteReviewPopupContent,
+      });
+      await voteReviewModal.present();
 
-    voteReviewModal.onDidDismiss();
-  }
+      voteReviewModal.onDidDismiss();
+   }
 
-  // todo: this doesn't actually do anything - can it be removed?
-  oneVoteClicked(contestNum: number) {
-    console.log('contest ' + contestNum + ' selected in oneVoteClicked');
-  }
+   // todo: this doesn't actually do anything - can it be removed?
+   oneVoteClicked(contestNum: number) {
+      console.log('contest ' + contestNum + ' selected in oneVoteClicked');
+   }
 
-  slideNext() {
-    this.slides.slideNext();
-    this.currentContest++;
-    this.currentContest = this.currentContest >= this.election.contests.length ? this.election.contests.length : this.currentContest;
-  }
+   slideNext() {
+      this.slides.slideNext();
+      this.currentContest++;
+      this.currentContest = this.currentContest >= this.election.contests.length ? this.election.contests.length : this.currentContest;
+   }
 
-  slidePrevious() {
-    this.slides.slidePrev();
-    this.currentContest--;
-    this.currentContest = this.currentContest <= 0 ? 1 : this.currentContest;
-  }
+   slidePrevious() {
+      this.slides.slidePrev();
+      this.currentContest--;
+      this.currentContest = this.currentContest <= 0 ? 1 : this.currentContest;
+   }
 
-  async settings(): Promise<void> {
-    const settingsPopupContent = { edFiles: this.edFiles, home: this };
-    const settingsModal = await this.modal.create({
-      component: SettingsPage,
-      componentProps: settingsPopupContent,
-    });
-    await settingsModal.present();
-    settingsModal.onDidDismiss();
-  }
+   async settings(): Promise<void> {
+      const settingsPopupContent = { edFiles: this.edFiles, home: this };
+      const settingsModal = await this.modal.create({
+         component: SettingsPage,
+         componentProps: settingsPopupContent,
+      });
+      await settingsModal.present();
+      settingsModal.onDidDismiss();
+   }
 
-  initialiseTranslation(): void {
-    this.translate.get('TITLE').subscribe((res: string) => {
-      this.title = res;
-    });
-    this.translate.get('description').subscribe((res: string) => {
-      this.description = res;
-    });
-    this.translate.get('TITLE_2', { value: 'John' }).subscribe((res: string) => {
-      this.titleTwo = res;
-    });
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    this.translate.get('data.name', { name_value: 'Marissa Mayer' }).subscribe((res: string) => {
-      this.name = res;
-    });
-  }
+   public changeLanguage(): void {
+      this.translateLanguage();
+   }
 
-  public changeLanguage(): void {
-    this.translateLanguage();
-  }
+   translateLanguage(): void {
+      this.translate.use(this.language);
+   }
 
-  translateLanguage(): void {
-    this.translate.use(this.language);
-    this.initialiseTranslation();
-  }
+   initTranslate(language) {
+      console.log('language is ' + language);
+      // Set the default language for translation strings, and the current language.
+      this.translate.setDefaultLang('en');
+      if (language) {
+         this.language = language;
+      } else {
+         // Set your language here
+         this.language = 'en';
+      }
+      this.translateLanguage();
+   }
 
-  initTranslate(language) {
-    console.log('language is ' + language);
-    // Set the default language for translation strings, and the current language.
-    this.translate.setDefaultLang('en');
-    if (language) {
-      this.language = language;
-    } else {
-      // Set your language here
-      this.language = 'en';
-    }
-    this.translateLanguage();
-  }
+   getDeviceLanguage() {
+      if (window.Intl && typeof window.Intl === 'object') {
+         this.initTranslate(navigator.language);
+      }
+   }
+   getTranslator() {
+      return this.translate;
+   }
 
-  getDeviceLanguage() {
-    if (window.Intl && typeof window.Intl === 'object') {
-      this.initTranslate(navigator.language);
-    }
-  }
-  getTranslator() {
-    return this.translate;
-  }
+   // todo: this doesn't actually do anything - can it be removed?
+   logScrollStart() {
+      console.log('home.page.ts:logScrollStart - in logScrollStart');
+   }
 
-  // todo: this doesn't actually do anything - can it be removed?
-  logScrollStart() {
-    console.log('home.page.ts:logScrollStart - in logScrollStart');
-  }
+   // todo: this doesn't actually do anything - can it be removed?
+   logScrolling() {
+      console.log('home.page.ts:logScrolling - in logScrolling');
+   }
 
-  // todo: this doesn't actually do anything - can it be removed?
-  logScrolling() {
-    console.log('home.page.ts:logScrolling - in logScrolling');
-  }
+   // todo: this doesn't actually do anything - can it be removed?
+   logScrollEnd() {
+      console.log('home.page.ts:logScrollEnd - in logScrollEnd');
+   }
 
-  // todo: this doesn't actually do anything - can it be removed?
-  logScrollEnd() {
-    console.log('home.page.ts:logScrollEnd - in logScrollEnd');
-  }
+   getEDFiles() {
+      try {
+         const xmlFiles = '/assets/data/index.txt';
 
-  getEDFiles() {
-    try {
-      const xmlFiles = '/assets/data/index.txt';
-
-      this.http
-        .get(xmlFiles, {
-          headers: new HttpHeaders()
+         this.http
+         .get(xmlFiles, {
+            headers: new HttpHeaders()
             .set('Content-Type', 'application/json ')
             .append('Access-Control-Allow-Methods', 'GET')
             .append('Access-Control-Allow-Origin', '*')
             .append(
-              'Access-Control-Allow-Headers',
-              'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method'
+               'Access-Control-Allow-Headers',
+               'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method'
             ),
-          responseType: 'text',
-        })
-        .subscribe((jsonData) => {
-          console.log('data read is ' + jsonData);
-          this.edFiles = jsonData.split('\n');
-        });
-    } catch (e) {
-      console.log('Error:', e);
-    }
-  }
-
-  getEDF() {
-    return this.xmlFile;
-  }
-
-  setEDF(xmlFile: string) {
-    this.xmlFile = xmlFile;
-    this.openXML();
-  }
-
-  itemClicked(event: Event, candidate: Candidate) {
-    console.log('got to itemClicked... checking whether ' + candidate.getCandidateName() + ' is a writein');
-    if (candidate.isWriteIn()) {
-      console.log('you clicked a write in');
-
-      this.writeInPopup(candidate);
-    }
-  }
-
-  async writeInPopup(candidate: Candidate): Promise<void> {
-    const writeinPopupContent = {
-      title: 'Write-In Candidate',
-      body: 'write-in election review goes here',
-      writeinName: candidate.getCandidateName(),
-    };
-
-    const writeinPopupModal = await this.modal.create({
-      component: WriteinPopupPage,
-      componentProps: writeinPopupContent,
-    });
-
-    await writeinPopupModal.present();
-
-    writeinPopupModal.onDidDismiss().then((data) => {
-      console.log('got this data ' + data);
-      if (data.data.trim().length > 0) {
-        candidate.setCandidateName(data.data);
-      } else {
-        candidate.setCandidateName(candidate.writeInConst);
+            responseType: 'text',
+         })
+         .subscribe((jsonData) => {
+            console.log('data read is ' + jsonData);
+            this.edFiles = jsonData.split('\n');
+         });
+      } catch (e) {
+         console.log('Error:', e);
       }
-    });
-  }
+   }
+
+   getEDF() {
+      return this.xmlFile;
+   }
+
+   setEDF(xmlFile: string) {
+      this.xmlFile = xmlFile;
+      this.openXML();
+   }
+
+   itemClicked(event: Event, candidate: Candidate) {
+      console.log('got to itemClicked... checking whether ' + candidate.getCandidateName() + ' is a writein');
+      if (candidate.isWriteIn()) {
+         console.log('you clicked a write in');
+
+         this.writeInPopup(candidate);
+      }
+   }
+
+   async writeInPopup(candidate: Candidate): Promise<void> {
+      const writeinPopupContent = {
+         title: 'Write-In Candidate',
+         body: 'write-in election review goes here',
+         writeinName: candidate.getCandidateName(),
+      };
+
+      const writeinPopupModal = await this.modal.create({
+         component: WriteinPopupPage,
+         componentProps: writeinPopupContent,
+      });
+
+      await writeinPopupModal.present();
+
+      writeinPopupModal.onDidDismiss().then((data) => {
+         console.log('got this data ' + data);
+         if (data.data.trim().length > 0) {
+            candidate.setCandidateName(data.data);
+         } else {
+            candidate.setCandidateName(candidate.writeInConst);
+         }
+      });
+   }
 }
--- src/classes/Contest.ts
+++ src/classes/Contest.ts
@@ -6,178 +6,191 @@ import { BallotSelection } from './BallotSelection';
 import { HomePage } from '../app/home/home.page';
 
 export class Contest {
-  readonly ballotSelectionQuery = '.BallotSelection[*]';
-  readonly nameQuery = '.Name[*]';
-  readonly votesAllowedQuery = '.VotesAllowed[0]';
-  public contestType: string;
-  public constestId: string;
-  public contestName: string;
-  public ballotSelections: BallotSelection[] = new Array();
-  public popupTitle = '';
-  public statusMessage1 = '';
-  public statusMessage2 = '';
-  public statusMessage3 = '';
-  private parent: Election;
-  private votesAllowed = 0;
-  private currentlySelected = 0;
-  private contestIndex = 0;
-  // todo: given that this same private variable is defined in multiple places, it should be hoisted to a
-  // shared scope and used everywhere it's needed. maybe an enum would be better here?
-  private writeIn = 'writein';
+   readonly ballotSelectionQuery = '.BallotSelection[*]';
+   readonly nameQuery = '.Name[*]';
+   readonly votesAllowedQuery = '.VotesAllowed[0]';
+   readonly contestIdQuery = '.objectId';
+   public contestType: string;
+   public contestId: string;
+   public contestName: string;
+   public ballotSelections: BallotSelection[] = new Array();
+   public popupTitle = '';
+   public statusMessage1 = '';
+   public statusMessage2 = '';
+   public statusMessage3 = '';
+   private parent: Election;
+   private votesAllowed = 0;
+   private currentlySelected = 0;
+   private contestIndex = 0;
+   // todo: given that this same private variable is defined in multiple places, it should be hoisted to a
+   // shared scope and used everywhere it's needed. maybe an enum would be better here?
+   private writeIn = 'writein';
 
-  constructor(public home: HomePage, aString: string, parent: Election, contestIndex: number) {
-    this.parent = parent;
-    this.contestIndex = contestIndex;
-    if (null != aString) {
-      try {
-        const values = jsonQuery(this.nameQuery, { data: aString }).value;
-        values.forEach((element) => {
-          //e.g. "President and Vice President"
-          this.contestName = element;
-        });
-        this.setBallotSelections(aString);
-        this.setVotesAllowed(aString);
-        this.home
-          .getTranslator()
-          .get('YOU_CAN_CHOOSE')
-          .subscribe((res: string) => {
-            this.statusMessage1 = res;
-          });
+   constructor(public home: HomePage, aString: string, parent: Election, contestIndex: number) {
+      console.log('aString is:' + aString);
+      this.parent = parent;
+      this.contestIndex = contestIndex;
+      if (null != aString) {
+         try {
+            const values = jsonQuery(this.nameQuery, { data: aString }).value;
+            values.forEach((element) => {
+               //e.g. "President and Vice President"
+               this.contestName = element;
+            });
+            this.setBallotSelections(aString);
+            this.setVotesAllowed(aString);
+            this.setContestId(aString);
+            this.home
+            .getTranslator()
+            .get('YOU_CAN_CHOOSE')
+            .subscribe((res: string) => {
+               this.statusMessage1 = res;
+            });
 
-        this.statusMessage2 = '' + (this.votesAllowed - this.currentlySelected);
-        this.home
-          .getTranslator()
-          .get('MORE')
-          .subscribe((res: string) => {
-            this.statusMessage3 = res;
-          });
-      } catch (e) {
-        console.log('Error:', e);
+            this.statusMessage2 = '' + (this.votesAllowed - this.currentlySelected);
+            this.home
+            .getTranslator()
+            .get('MORE')
+            .subscribe((res: string) => {
+               this.statusMessage3 = res;
+            });
+         } catch (e) {
+            console.log('Error:', e);
+         }
       }
-    }
-  }
+   }
 
-  getContestName(): string {
-    return this.contestName;
-  }
+   getContestName(): string {
+      return this.contestName;
+   }
 
-  setBallotSelections(aString: string) {
-    try {
-      const values = jsonQuery(this.ballotSelectionQuery, { data: aString }).value;
-      values.forEach((element) => {
-        this.ballotSelections.push(new BallotSelection(element, this));
-      });
-      //add a writein
-      const aBallotSelection = new BallotSelection(this.writeIn, this);
-      this.ballotSelections.push(aBallotSelection);
-    } catch (e) {
-      console.log('Error:', e);
-    }
-  }
+   setBallotSelections(aString: string) {
+      try {
+         const values = jsonQuery(this.ballotSelectionQuery, { data: aString }).value;
+         values.forEach((element) => {
+            this.ballotSelections.push(new BallotSelection(element, this));
+         });
+         //add a writein
+         const aBallotSelection = new BallotSelection(this.writeIn, this);
+         this.ballotSelections.push(aBallotSelection);
+      } catch (e) {
+         console.log('Error:', e);
+      }
+   }
 
-  setVotesAllowed(aString: string) {
-    try {
-      this.votesAllowed = jsonQuery(this.votesAllowedQuery, { data: aString }).value;
-    } catch (e) {
-      console.log('Error:', e);
-    }
-  }
+   setContestId(aString: string) {
+      try {
+         this.contestId = jsonQuery(this.contestIdQuery, { data: aString }).value;
+      } catch (e) {
+         console.log('Error:', e);
+      }
 
-  getVotesAllowed(): number {
-    return this.votesAllowed;
-  }
+   }
 
-  getPage(): string {
-    return this.contestIndex + 1 + '/' + this.parent.getContestNamesCount();
-  }
+   setVotesAllowed(aString: string) {
+      try {
+         this.votesAllowed = jsonQuery(this.votesAllowedQuery, { data: aString }).value;
+      } catch (e) {
+         console.log('Error:', e);
+      }
+   }
 
-  getCurrentlySelected(): number {
-    return this.currentlySelected;
-  }
+   getVotesAllowed(): number {
+      return this.votesAllowed;
+   }
 
-  getVotesLeft(): number {
-    return this.votesAllowed - this.currentlySelected;
-  }
+   getPage(): string {
+      return this.contestIndex + 1 + '/' + this.parent.getContestNamesCount();
+   }
 
-  getBallotSelections(): BallotSelection[] {
-    return this.ballotSelections;
-  }
+   getCurrentlySelected(): number {
+      return this.currentlySelected;
+   }
 
-  getParent(): Election {
-    return this.parent;
-  }
+   getVotesLeft(): number {
+      return this.votesAllowed - this.currentlySelected;
+   }
 
-  ionChangeIgnoreCheckbox(cbox: Checkbox) {
-    //always want true in the vote review page - don't allow deselection there!
-    console.log('Contest.ts: inside ionChangeIgnoreCheckbox, rechecking the checkbox');
-    cbox.checked = true;
-  }
+   getBallotSelections(): BallotSelection[] {
+      return this.ballotSelections;
+   }
 
-  ionChangeUpdateCheckboxAlt(cbox) {
-    console.log(
-      'Contest.ts: in updatecheckbox-ALT, cbox is:' + cbox.currentTarget.checked + ' currentlySelected is ' + this.currentlySelected
-    );
-  }
+   getParent(): Election {
+      return this.parent;
+   }
 
-  ionChangeUpdateCheckbox(cbox) {
-    console.log('Contest.ts: in updatecheckbox, cbox is:' + cbox.currentTarget.checked + ' currentlySelected is ' + this.currentlySelected);
-    console.log(cbox.currentTarget);
+   ionChangeIgnoreCheckbox(cbox: Checkbox) {
+      //always want true in the vote review page - don't allow deselection there!
+      console.log('Contest.ts: inside ionChangeIgnoreCheckbox, rechecking the checkbox');
+      cbox.checked = true;
+   }
 
-    if (cbox.currentTarget.checked) {
-      console.log('Contest.ts: cbox checked');
-      this.currentlySelected++;
-    } else {
-      console.log('Contest.ts: cbox was UNchecked');
-      this.currentlySelected--;
-    }
+   ionChangeUpdateCheckboxAlt(cbox) {
+      console.log(
+         'Contest.ts: in updatecheckbox-ALT, cbox is:' + cbox.currentTarget.checked + ' currentlySelected is ' + this.currentlySelected
+      );
+   }
 
-    console.log('Contest.ts: currently selected: ' + this.currentlySelected + ', votesAllowed: ' + this.votesAllowed);
+   ionChangeUpdateCheckbox(cbox) {
+      console.log('Contest.ts: in updatecheckbox, cbox is:' + cbox.currentTarget.checked + ' currentlySelected is ' + this.currentlySelected);
+      console.log(cbox.currentTarget);
 
-    if (this.currentlySelected === this.votesAllowed) {
-      this.statusMessage1 = '';
-      this.statusMessage2 = '';
-      this.statusMessage3 = '';
-    } else if (this.votesAllowed > this.currentlySelected) {
-      this.home
-        .getTranslator()
-        .get('YOU_CAN_CHOOSE')
-        .subscribe((res: string) => {
-          this.statusMessage1 = res;
-        });
-      this.statusMessage2 = '' + (this.votesAllowed - this.currentlySelected);
-      this.home
-        .getTranslator()
-        .get('MORE')
-        .subscribe((res: string) => {
-          this.statusMessage3 = res;
-        });
-    } else {
-      this.home
-        .getTranslator()
-        .get('SELECTED_TOO_MANY')
-        .subscribe((res: string) => {
-          this.statusMessage1 = res;
-        });
+      if (cbox.currentTarget.checked) {
+         console.log('Contest.ts: cbox checked');
+         this.currentlySelected++;
+      } else {
+         console.log('Contest.ts: cbox was UNchecked');
+         this.currentlySelected--;
+      }
 
-      this.home
-        .getTranslator()
-        .get('TOO_MANY_TITLE')
-        .subscribe((res: string) => {
-          this.popupTitle = res;
-        });
-      const popupContent = { title: this.popupTitle, body: this.statusMessage1 };
-      //deselect what the user just did
-      cbox.currentTarget.checked = false;
-      //pop up modal dialog telling user to deselect something - they're already at max
-      this.home.openIonModal(popupContent);
-    }
-  }
+      console.log('Contest.ts: currently selected: ' + this.currentlySelected + ', votesAllowed: ' + this.votesAllowed);
 
-  canSelectMoreCandidates(): boolean {
-    return this.votesAllowed !== this.currentlySelected;
-  }
+      if (this.currentlySelected == this.votesAllowed) {
+         this.statusMessage1 = '';
+         this.statusMessage2 = '';
+         this.statusMessage3 = '';
+      } else if (this.votesAllowed > this.currentlySelected) {
+         console.log('Contest.ts - got here!!!!');
+         this.home
+         .getTranslator()
+         .get('YOU_CAN_CHOOSE')
+         .subscribe((res: string) => {
+            this.statusMessage1 = res;
+         });
+         this.statusMessage2 = '' + (this.votesAllowed - this.currentlySelected);
+         this.home
+         .getTranslator()
+         .get('MORE')
+         .subscribe((res: string) => {
+            this.statusMessage3 = res;
+         });
+      } else {
+         this.home
+         .getTranslator()
+         .get('SELECTED_TOO_MANY')
+         .subscribe((res: string) => {
+            this.statusMessage1 = res;
+         });
 
-  oneVoteClicked() {
-    console.log('got here!');
-  }
+         this.home
+         .getTranslator()
+         .get('TOO_MANY_TITLE')
+         .subscribe((res: string) => {
+            this.popupTitle = res;
+         });
+         const popupContent = { title: this.popupTitle, body: this.statusMessage1 };
+         //deselect what the user just did
+         cbox.currentTarget.checked = false;
+         //pop up modal dialog telling user to deselect something - they're already at max
+         this.home.openIonModal(popupContent);
+      }
+   }
+
+   canSelectMoreCandidates(): boolean {
+      return this.votesAllowed !== this.currentlySelected;
+   }
+
+   oneVoteClicked() {
+      console.log('got here!');
+   }
 }
